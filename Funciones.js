b6d021a7733b45fc4376d03dbd121904e39bd6f9
JuAN-AnDRex <92338151+JuAN-AnDRex@users.noreply.github.com>
2021-12-03 13:14:33 -0600

    Add files via upload

---
+++ Funciones.js
@@ -0,0 +1,28 @@
+function Sumar() {
+    alert("vas a sumar");
+    var Nu1 = document.getElementById("N1").value;
+    var Nu2 = document.getElementById("N2").value;
+    var Res = parseInt(Nu1)+parseInt(Nu2);
+    alert("el resultado es de = "+Res);
+}
+function Restar() {
+    alert("vas a restar");
+    var Nu1 = document.getElementById("N1").value;
+    var Nu2 = document.getElementById("N2").value;
+    var Res = parseInt(Nu1)-parseInt(Nu2);
+    alert("el resultado es de = "+Res);
+}
+function Multiplicar() {
+    alert("vas a multiplicar");
+    var Nu1 = document.getElementById("N1").value;
+    var Nu2 = document.getElementById("N2").value;
+    var Res = parseInt(Nu1)*parseInt(Nu2);
+    alert("el resultado es de = "+Res);
+}
+function Dividir() {
+    alert("vas a dividir");
+    var Nu1 = document.getElementById("N1").value;
+    var Nu2 = document.getElementById("N2").value;
+    var Res = parseInt(Nu1)/parseInt(Nu2);
+    alert("el resultado es de = "+Res);
+}
